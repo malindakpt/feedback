bb5a66c2cb39db28d0804f2974813435410df7a7
IT21823770 <110513371+IT21823770@users.noreply.github.com>
2024-08-14 15:59:19 +0530

    fix errors and create .env

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,15 +1,14 @@
 import { getFirestore } from "firebase/firestore";
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyANQgqruX24y7QdYBwUQNjfGgJ2kYgu_LE",
-  authDomain: "feedback-60b9d.firebaseapp.com",
-  projectId: "feedback-60b9d",
-  storageBucket: "feedback-60b9d.appspot.com",
-  messagingSenderId: "795367922413",
-  appId: "1:795367922413:web:ca7ebaca938b8ca06e8ae4"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
