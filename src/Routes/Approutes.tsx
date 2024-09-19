969b99f8f132d8883d2aa771ca394527cc2ed822
manjitha-dev <manjithakpt@gmail.com>
2024-09-19 18:46:50 +0530

    dashboard updates

--- src/Routes/Approutes.tsx
+++ src/Routes/Approutes.tsx
@@ -10,6 +10,7 @@ import BranchView from '../components/Admin/branchView';
 import CompanyView from '../components/Admin/companyView';
 import BranchStatView from '../components/Admin/SingleView/branchStatView';
 import EmployeeStatView from '../components/Admin/SingleView/employeeStatView';
+import Dashboard from '../components/Admin/Dashboard';
 
 // USER
 import BranchFeedback from '../components/user/branchFeedback';
@@ -31,6 +32,7 @@ const AppRoutes: React.FC = () => {
           
           {/* Protected Admin Routes */}
           <Route element={<PrivateRoute allowedRoles={['admin', 'superAdmin']} />}>
+            <Route path="dashboard" element={<Dashboard />} />
             <Route path="branch-manager" element={<BranchManager />} />
             <Route path="branchView" element={<BranchView />} />
             <Route path="companyView" element={<CompanyView />} />
