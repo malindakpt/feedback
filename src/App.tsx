135408a8913aa2081b1053945ffd50b56a73cf75
it21827976 <110651727+it21827976@users.noreply.github.com>
2024-09-24 18:48:04 +0530

    Clear All Unnecessary files in InputFields

--- src/App.tsx
+++ src/App.tsx
@@ -1,85 +1,10 @@
 import React from 'react';
-import './App.css';
-import EmpImage from './Components/EmpImage';
-import EmpList from './Components/EmpList';
-
-
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import CompanyView from './Components/CompanyView';
-import BranchView from './Components/BranchView';
-import EmployeeView from './Components/EmployeeView';
-import User from './Components/User/User';
-import UserDetails from './Components/User/UserDetails';
-import AddCompany from './Components/AddCompany';
-import ViewCompanies from './Components/ViewCompanies';
-
-
 
 function App() {
-  
-  const employeeobject = {
-    empId: 'E001',
-    empName: 'Nithila',
-    imageUrl: 'https://randomuser.me/api/portraits/men/81.jpg',
-  }
-
-  const employees = [
-    {
-      empId: 'E001',
-      empName: 'Kasun Dias',
-      imageUrl: 'https://randomuser.me/api/portraits/men/81.jpg',
-    },
-    {
-      empId: 'E002',
-      empName: 'Shehani Peiris',
-      imageUrl: 'https://randomuser.me/api/portraits/women/21.jpg',
-    },
-    {
-      empId: 'E003',
-      empName: 'Lego Man',
-      imageUrl: 'https://randomuser.me/api/portraits/lego/5.jpg',
-    },
-    {
-      empId: 'E004',
-      empName: 'Chef Kiththa',
-      imageUrl: 'https://randomuser.me/api/portraits/lego/8.jpg',
-    },
-
-
-  ]
-
-
   return (
-    <div>
-      <BrowserRouter>
-        <Routes>
-          <Route
-            path="/empimage"
-            element={
-              <EmpImage
-                 {...employeeobject}
-                
-              />
-            }
-          />
-          <Route path='/emplist' element={<EmpList employees={employees}/>} />
-          
-          <Route path='/:companyId' element={<CompanyView/>} />
-          <Route path='/:companyId/:branchId' element={<BranchView/>} />
-          <Route path='/:companyId/:branchId/:employeeId' element={<EmployeeView/>} />
-
-          <Route path='/users' element={<User/>} />
-          <Route path='/users/:id' element={<UserDetails/>} />
-
-          <Route path='/addcompany' element={<AddCompany/>} />
-          <Route path='/viewcompany' element={<ViewCompanies/>} />
-          
-        </Routes>
-      </BrowserRouter>
+    <div className="App">
+      
     </div>
-
-
-    
   );
 }
 
