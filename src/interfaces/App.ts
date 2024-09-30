59c0d63067c9012a02fa56763362779aa96f717e
Ayodhya-98 <ayodyanirmanirm@gmail.com>
2024-09-30 18:22:09 +0530

    remove other attributes except user

--- src/interfaces/App.ts
+++ src/interfaces/App.ts
@@ -2,9 +2,6 @@ import { User } from './User';
 
 export interface AppState {
   user: User | null;
-  isAuthenticated: boolean;
-  status: 'idle' | 'loading' | 'failed';
-  error: string | null;
 }
 
 
