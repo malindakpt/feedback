e9ae7215f45f1165ec33be94aaf6c197ba2c58c5
IT21823770 <110513371+IT21823770@users.noreply.github.com>
2024-10-11 20:09:30 +0530

    updated

--- src/components/Shared/charts.tsx
+++ src/components/Shared/charts.tsx
@@ -1,12 +1,48 @@
 import React from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  PointElement,
+  LineElement,
+  ArcElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
 import { Chart } from 'react-chartjs-2';
-import { commonOptions } from './chartOptions';
 
+// Register the necessary components from Chart.js
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  PointElement,
+  LineElement,
+  ArcElement,
+  Title,
+  Tooltip,
+  Legend
+);
 interface ChartComponentProps {
   title: string;
   data: any;
   showTitle?: boolean;
 }
+
+const commonOptions = (showTitle: boolean, title: string) => ({
+  responsive: true,
+  plugins: {
+    legend: {
+      display: true,
+    },
+    title: {
+      display: showTitle,
+      text: title,
+    },
+  },
+});
 
 const ChartComponent: React.FC<ChartComponentProps> = ({ title, data, showTitle = true }) => {
   const { chartType } = data;
