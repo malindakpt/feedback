0152b5257299600cc98c2c223dd185ef4b2c22d1
it21827976 <110651727+it21827976@users.noreply.github.com>
2024-11-29 20:24:07 +0530

    remove unwanted file and error fix app

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import ExamplePage from './components/examplePage';
 // import { BrowserRouter,Router, Route, Routes } from 'react-router-dom';
-// import AppRoutes from './routes/appRoutes';
+import AppRoutes from './routes/appRoutes';
 const App: React.FC = () => {
   return (
       
     <div className="App">
-    {/* <AppRoutes /> */}
-    <ExamplePage/>
+    <AppRoutes />
     </div>
   );
 }
