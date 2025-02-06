9205ea1a0c9f5334406203fe8a81c054ed11d654
Ashan Pathiraja <it21823770@my.sliit.lk>
2025-02-06 20:39:30 +0530

    Update addEmployee.tsx

--- src/components/admin/addEmployee/addEmployee.tsx
+++ src/components/admin/addEmployee/addEmployee.tsx
@@ -37,20 +37,17 @@ const AddEmployeeForm: React.FC<AddEmployeeFormProps> = ({
           <TextInput
             label="User Id"
             name="uid"
-            required
             errorText={touched.uid && errors.uid ? errors.uid : ""}
           />
           <TextInput
             label="First Name"
             name="firstName"
-            required
             disabled={!values.uid || disabled}
             errorText={touched.firstName && errors.firstName ? errors.firstName : ""}
           />
           <TextInput
             label="Last Name"
             name="lastName"
-            required
             disabled={!values.firstName || disabled}
             errorText={touched.lastName && errors.lastName ? errors.lastName : ""}
           />
@@ -58,7 +55,6 @@ const AddEmployeeForm: React.FC<AddEmployeeFormProps> = ({
             label="Company"
             name="companyId"
             options={companyOptions}
-            required
             disabled={!values.lastName || disabled}
             onChange={(name, newValue) => {
               setFieldValue(name, newValue);
@@ -70,35 +66,30 @@ const AddEmployeeForm: React.FC<AddEmployeeFormProps> = ({
             label="Branch"
             name="branchId"
             options={branchOptions}
-            required
             disabled={!values.companyId || disabled}
             onChange={(name, newValue) => setFieldValue(name, newValue)}
           />
           <TextInput
             label="Position"
             name="position"
-            required
             disabled={!values.branchId || disabled}
             errorText={touched.position && errors.position ? errors.position : ""}
           />
           <DateInput
             label="Birthday"
             name="birthday"
-            required
             disabled={!values.position || disabled}
             errorText={touched.birthday && errors.birthday ? errors.birthday : ""}
           />
           <TextInput
             label="NIC"
             name="nic"
-            required
             disabled={!values.birthday || disabled}
             errorText={touched.nic && errors.nic ? errors.nic : ""}
           />
           <TextInput
             label="Email"
             name="email"
-            required
             disabled={!values.nic || disabled}
             errorText={touched.email && errors.email ? errors.email : ""}
           />
