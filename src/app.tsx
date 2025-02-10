ad21f77868ceee01b70f8f4bc3d4806dfa1f9722
it21827976 <110651727+it21827976@users.noreply.github.com>
2025-02-10 12:22:28 +0530

    new changes and error fix

--- src/app.tsx
+++ src/app.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
 // // import { BrowserRouter,Router, Route, Routes } from 'react-router-dom';
-// import AppRoutes from './routes/appRoutes';
-// import QrCode from './components/qrCode/qrCode';
-import Example from './components/qrCode/example';
+import AppRoutes from './routes/appRoutes';
+
 
 
 const App: React.FC = () => {
   return (
       
     <div className="App">
-    {/* <AppRoutes /> */}
-    <Example/>
-    
+    <AppRoutes />
     </div>
   );
 }
