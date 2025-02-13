13a236c060a0d701d4ada009c01639118d5de2e8
manjitha-dev <manjithakpt@gmail.com>
2025-02-13 16:11:10 +0530

    type change

--- src/interfaces/reviewFilter.ts
+++ src/interfaces/reviewFilter.ts
@@ -1,6 +1,6 @@
 export interface ReviewFilter {
-    minRating: string;
-    maxRating: String;
+    minRating: number;
+    maxRating: number;
     fromDate:Date;
     toDate:Date;
     userId:string;
